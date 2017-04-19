59ae8e0207d079a664d42c8eec2dc519f2e5fa13
AlSidorenko <alek.sidorenko1979@gmail.com>
2017-04-19 12:13:06 +0300

    task 4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,20 @@
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Aleks Sidorenko (alek.sidorenko1979@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+ 
 public class Calculate {
 	public static void main(String[] args) {
 		
+	/**
+     * Main.
+     * @param args - args.
+     */
 		System.out.println("Hello world");
 	}
 }
