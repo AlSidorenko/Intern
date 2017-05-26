63d3bbf7ece5707caf873d185747bc386735a529
AlSidorenko <alek.sidorenko1979@gmail.com>
2017-05-26 11:43:42 +0300

    task 195

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+/**
+* Package for Array.
+*
+* @author Aleks Sidorenko (mailto:alek.sidorenko1979@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+public class BubbleSort {
+	/**
+	 *Sort - array.
+	 *@param array - source array.
+	 *@return - sort array.
+	 */
+    public int[] sort(int[] array) {
+        int t;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = array.length - 1; j >= i; j--) {
+                if (array[j - 1] > array[j]) {
+                    t = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = t;
+                }
+            }
+        }
+        return array;
+    }
+}
