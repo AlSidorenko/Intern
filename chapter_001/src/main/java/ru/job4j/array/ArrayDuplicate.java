08868c3c69747eae3d76e86c4554606d233bae71
AlSidorenko <alek.sidorenko1979@gmail.com>
2017-05-31 00:06:24 +0300

    task 225

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,34 @@
+﻿package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+* Package for Array.
+*
+* @author Aleks Sidorenko (mailto:alek.sidorenko1979@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+public class ArrayDuplicate {
+	/**
+	 *remove - remove elements of array.
+	 *@param array - source array.
+	 *@return - array.
+	 */
+    public String[] remove(String[] array) {
+        int countRepeatElement = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    countRepeatElement++;
+                }
+            }
+        }
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (array[i] != array[j]) {
+                array[j++] = array[i];
+            }
+        }
+        return Arrays.copyOf(array, array.length - countRepeatElement);
+    }
+}
