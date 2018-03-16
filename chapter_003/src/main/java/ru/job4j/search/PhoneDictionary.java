6bbda8c4e6fc40b34b1e7f1e4cbc8c51ddb3a983
AlSidorenko <alek.sidorenko1979@gmail.com>
2018-03-16 18:15:18 +0200

    Телефонный справочник на базе ArrayList [#41598]

---
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,57 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created on 21.02.2018.
+ *
+ * @author Aleks Sidorenko (alek.sidorenko1979@gmail.com).
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class PhoneDictionary {
+
+    /**
+     * List of Person.
+     */
+    private List<Person> persons = new ArrayList<Person>();
+
+    /**
+     * Method adding item.
+     * @param person - list of person.
+     */
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+        for (int i = 0; i < persons.size(); i++) {
+            if (persons.get(i) != null & persons.get(i).getName().contains(key)) {
+                 result.add(persons.get(i));
+            } else if (persons.get(i) != null & persons.get(i).getSurname().contains(key)) {
+                result.add(persons.get(i));
+            } else if (persons.get(i) != null & persons.get(i).getPhone().contains(key)) {
+                result.add(persons.get(i));
+            } else if (persons.get(i) != null & persons.get(i).getAddress().contains(key)) {
+                result.add(persons.get(i));
+            } else {
+                result.add(null);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneDictionary{"
+                + "persons= " + persons + '}';
+    }
+}
