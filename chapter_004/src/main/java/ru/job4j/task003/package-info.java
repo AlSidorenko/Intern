5c249a0da4f32032e99ac85f3b2f3a5a7ed74bbc
AlSidorenko <alek.sidorenko1979@gmail.com>
2018-10-18 11:57:46 +0300

    Preparing the code for task 3. [# 24256]

---
+++ chapter_004/src/main/java/ru/job4j/task003/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Created on 17.12.2017.
+ *
+ * @author Aleks Sidorenko (alek.sidorenko1979@gmail.com).
+ * @version $Id$.
+ * @since 0.1.
+ */
+package ru.job4j.task003;
+
+/*
+3. В задании Трекере в классе Tracker переделать поиск на lambda. [#24256]
+  Петр Арсентьев,  10.09.18 5:23
+В задании Трекере в классе Tracker переделать поиск на lambda.
+Использовать функции Predicate.
+ */
---
+++ chapter_004/src/test/java/ru/job4j/task003/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created on 18.10.2018.
+ *
+ * @author Aleks Sidorenko (alek.sidorenko1979@gmail.com).
+ * @version $Id$.
+ * @since 0.1.
+ */
+package ru.job4j.task003;
