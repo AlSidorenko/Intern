f3b81bf84cb5dada3a016d8d048f0b1a830d8a5e
AlSidorenko <alek.sidorenko1979@gmail.com>
2018-11-08 15:50:22 +0200

    Preparing to complete assignment [#24259]

---
+++ chapter_004/src/test/java/ru/job4j/streamapi/task001/transformations/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created on 01.05.2018.
+ *
+ * @author Aleks Sidorenko (alek.sidorenko1979@gmail.com).
+ * @version $Id$.
+ * @since 0.1.
+ */
+package ru.job4j.streamapi.task001.transformations;
