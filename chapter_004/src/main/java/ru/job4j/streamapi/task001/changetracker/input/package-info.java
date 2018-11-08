15441a2be198b3ff668fe02d4a4ab5159b305598
AlSidorenko <alek.sidorenko1979@gmail.com>
2018-11-08 17:29:39 +0200

    Preparing to complete assignment [#24259]

--- chapter_004/src/main/java/ru/job4j/streamapi/task001/changetracker/input/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created on 17.12.2017.
- *
- * @author Aleks Sidorenko (alek.sidorenko1979@gmail.com).
- * @version $Id$.
- * @since 0.1.
- */
-package ru.job4j.streamapi.task001.changetracker.input;
